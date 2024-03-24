1685b2cbb834a05527069cae935c1ba69c090b60
iamhrk <iamhrk8@gmail.com>
2024-03-24 17:23:08 +0530

    add product list component methods

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -11,7 +11,21 @@ export class ProductsListComponent {
   @Input() productList: Product[];
   @Output() onProductSelected : EventEmitter<Product>;
 
+  private currentProduct: Product;
+
   constructor() {
-    this.onProductSelected = new EventEmitter();di
+    this.onProductSelected = new EventEmitter();
+  }
+
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean {
+    if(!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
   }
 }
