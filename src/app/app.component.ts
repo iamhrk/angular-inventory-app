bc06b6e19d13e19a417bc02c2fccf464dc04f674
iamhrk <iamhrk8@gmail.com>
2024-03-24 11:56:26 +0530

    Add more products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,32 @@ import { Product } from './models/product.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  product : Product;
+  products : Product[];
   constructor() {
-    this.product = new Product(
-    "NICEHAT",
-    "A Nice Black Hat",
-    "/assets/images/products/black-hat.jpg",
-    ["Men", "Accessories", "Hats"], 
-    29.99)
+    this.products =[
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99
+      ),
+
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99
+      ),
+
+      new Product(
+        "NICEHAT",
+        "A Nice Black Hat",
+        "/assets/images/products/black-hat.jpg",
+        ["Men", "Accessories", "Hats"],
+        29.99
+      )
+    ]
   }
 }
